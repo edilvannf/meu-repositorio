1f5d26fb3cb2572c01a87d5d93cf7885f5f5ee14
Edilvan Ferreira <126162436+jirvanNJR@users.noreply.github.com>
2024-03-26 16:35:13 -0300

    temporizador

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
 const botoes = document.querySelectorAll(".botao");
-        const cronometroElement = document.getElementById("cronometro");
-        let segundos = 0;
-        let minutos = 0;
-        let horas = 0;
-        let cronometroInterval;
+const cronometroElement = document.getElementById("cronometro");
+let segundos = 600; // Tempo inicial em segundos (10 minutos)
+let cronometroInterval;
 
-        function atualizarCronometro() {
-            segundos++;
-            if (segundos >= 60) {
-                segundos = 0;
-                minutos++;
-                if (minutos >= 60) {
-                    minutos = 0;
-                    horas++;
-                }
-            }
-            cronometroElement.textContent = `${horas < 10 ? '0' + horas : horas}:${minutos < 10 ? '0' + minutos : minutos}:${segundos < 10 ? '0' + segundos : segundos}`;
+function atualizarCronometro() {
+    const minutos = Math.floor(segundos / 60);
+    const segundosRestantes = segundos % 60;
+    cronometroElement.textContent = `${minutos < 10 ? '0' + minutos : minutos}:${segundosRestantes < 10 ? '0' + segundosRestantes : segundosRestantes}`;
+    if (segundos === 0) {
+        clearInterval(cronometroInterval);
+    } else {
+        segundos--;
+    }
+}
+
+cronometroInterval = setInterval(atualizarCronometro, 1000);
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
         }
-
-        cronometroInterval = setInterval(atualizarCronometro, 1000);
-
-        for (let i = 0; i < botoes.length; i++) {
-            botoes[i].onclick = function () {
-                for (let j = 0; j < botoes.length; j++) {
-                    botoes[j].classList.remove("ativo");
-                }
-                botoes[i].classList.add("ativo");
-            }
-        }
+        botoes[i].classList.add("ativo");
+    }
+}
