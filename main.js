c0210b8cd38169abd8293aa090341b9eba6bace9
Edilvan Ferreira <126162436+jirvanNJR@users.noreply.github.com>
2024-04-25 16:25:48 -0300

    tempo objtvs

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ const tempoObjetivo2 = new Date("2024-05-29T00:00:00");
 const tempoObjetivo3 = new Date("2024-05-29T00:00:00");
 const tempoObjetivo4 = new Date("2024-05-29T00:00:00");
 
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
